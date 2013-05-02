ff97f1479dcca235884da9757702f3725e459c7c
Lachezar Yankov <lachezar.yankov@lendo.se>
2013-05-02 22:50:26 +0300

    It took me some time before I realize that jinja is processing the template tags for mustache.js - well, it is fixed now (mustache templates are wrapped in  tags). Also htmlmin was going mental because of not processed jinja tags which are violating html format. So, no minification of jinja html pages.

--- todo.py
+++ todo.py
@@ -1,30 +1,25 @@
 from flask import Flask, render_template
 from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext.restless import APIManager
+from flask.ext.restless import APIManager, helpers
 from models import some_lame_dependancy_here
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 db = SQLAlchemy(app)
-Task = some_lame_dependancy_here(db)['Task']
+Task = some_lame_dependancy_here(db)['Task'] # how to get rid of this :/
 manager = APIManager(app, flask_sqlalchemy_db=db)
-manager.create_api(Task) # how to get rid of this :/
+manager.create_api(Task, methods=['GET', 'POST', 'PUT'])
 
 @app.route('/')
 def index():
     tasks = Task.query.all()
-    return render_template('index.html', tasks=tasks)
+    tasks_dict = map(helpers.to_dict, tasks)
+    return render_template('index.html', tasks=tasks, tasks_dict=tasks_dict)
 
 if __name__ == '__main__':
+    debug = True
     
     db.create_all()
-    app.run(debug=True)
+    app.run(debug=debug)
 
-    # serve static files from the root in debug mode
-    if app.config['DEBUG']:
-        from werkzeug import SharedDataMiddleware
-        import os
-        app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
-          '/assets/scripts/': os.path.join(os.path.dirname(__file__), 'scripts'),
-          '/assets/styles/': os.path.join(os.path.dirname(__file__), 'styles'),
-        })
+

a3c87e44d9af952a99353007eb07351bc41b2ca3
Lachezar Yankov <lachezar.yankov@lendo.se>
2013-05-04 18:18:34 +0300

    added heroku build grunt task

--- todo.py
+++ todo.py
@@ -19,7 +19,7 @@ if os.environ.get('HEROKU_POSTGRESQL_AMBER_URL'):
 else:
     run_config['debug'] = True
     app = Flask(__name__, static_folder='devstatic', template_folder='devtemplates')
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['HEROKU_POSTGRESQL_AMBER_URL']
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 
 db = SQLAlchemy(app)
 Task = some_lame_dependancy_here(db)['Task'] # how to get rid of this :/
